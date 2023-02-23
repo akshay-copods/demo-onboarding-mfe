3de7399c2483de2871d6f45b2ad6295d31a933c2
Akshay Kamble <akshay.kamble@copods.co>
2023-02-23 12:15:43 +0530

    update public path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports =(env,argv)=> ({
   output: {
-    publicPath: "http://localhost:3001/",
+    publicPath: `${argv.mode === "production"?'https://gentle-melomakarona-3566dd.netlify.app/':'http://localhost:3001/'}`,
   },
 
   resolve: {
